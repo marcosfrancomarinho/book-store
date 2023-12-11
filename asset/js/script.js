ee3eedcb7a0289b06e15740f57631b08309e66ab
marcosmarinho <139090070+marcosfrancomarinho@users.noreply.github.com>
2023-12-10 21:29:33 -0300

    Update script.js

--- asset/js/script.js
+++ asset/js/script.js
@@ -14,7 +14,7 @@ $(window).ready(async () => {
     scrollPage()
 })
 async function dataJson() {
-    const response = await fetch("https://server-books-git-main-marcosfrancomarinho.vercel.app/books")
+    const response = await fetch("https://server-book.vercel.app/books")
     if (response.status != 404) {
         return response.json()
     }
