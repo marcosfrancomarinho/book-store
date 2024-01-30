01a9677a19764386039fdf52a4b3bd77692f3c38
marcosmarinho <139090070+marcosfrancomarinho@users.noreply.github.com>
2024-01-30 11:30:15 -0300

    Update script.js

--- shopping/js/script.js
+++ shopping/js/script.js
@@ -9,7 +9,7 @@ $(window).ready(async () => {
     create.bind(object[idx])(object)
 })
 async function dataJson() {
-    const response = await fetch("https://server-book.vercel.app/books")
+    const response = await fetch("https://server-lovat-mu.vercel.app/books")
     if (response.status != 404) {
         return response.json()
     }
